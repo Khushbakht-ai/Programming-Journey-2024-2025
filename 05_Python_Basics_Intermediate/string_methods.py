c999e85000ca83d48dcb58cf44c127676eb4ce44
Khushbakht-ai <khushi.ai.s19@gmail.com>
2025-06-18 12:42:44 +0500

    Built-in string functions – April 2025

---
+++ 05_Python_Basics_Intermediate/string_methods.py
@@ -0,0 +1,6 @@
+text = " Khushi Loves Python "
+print("Original:", text)
+print("Stripped:", text.strip())
+print("Upper:", text.upper())
+print("Lower:", text.lower())
+print("Replace:", text.replace("Python", "Coding"))
